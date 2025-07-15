fe7640da03a3fa6e9188dd6d5a1dfe295e0a702e
Hrithik-Bhattacharya <hrithikbhattacharya6@gmail.com>
2025-07-15 19:23:49 +0530

    Add files via upload
    Shape detection extended for ID cards

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+def is_dark_rectangle(frame, contour):
+    mask = np.zeros(frame.shape[:2], dtype=np.uint8)
+    cv2.drawContours(mask, [contour], -1, 255, -1)
+    mean_val = cv2.mean(frame, mask=mask)
+    brightness = sum(mean_val[:3]) / 3  
+    return brightness < 50 
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blur, 100, 350)
+
+    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True)
+        if len(approx) == 4 and cv2.isContourConvex(approx):
+            if is_dark_rectangle(frame, approx):
+                 color = (0, 0, 255)
+            else:
+                color=(0,255,0)
+            cv2.drawContours(frame, [approx], 0, color, 3)
+
+    cv2.imshow("Rectangle Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
